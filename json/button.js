364ec957f2dd3c964ff402458967b7996c4b645d
MMisk <A230427MM@rvt.lv^C>
2025-04-20 13:34:28 +0300

    izmaiņas

--- json/button.js
+++ json/button.js
@@ -1,6 +1,6 @@
 function Dowloadzip() {
     // Failu.zip vajag pievienot projekta mape dowloadzip
-    const zipUrl = 'dowloadzip/LibaryManager.zip'; // faila atrodašanas vieta
+    const zipUrl = 'dowloadzip/LibaryManager-1.zip'; // faila atrodašanas vieta
     const a = document.createElement('a');
     a.href = zipUrl;
     a.download = 'LibaryManager.zip'; // faila nosaukums 
